2c675829f0ec5799f25fcefc23a9255466116ee6
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2018-03-19 12:28:46 -0300

    add working & course props

--- models/Coder.js
+++ models/Coder.js
@@ -12,25 +12,13 @@ Coder.add({
   github: { type: Types.Url, default: '' },
   linkedin: { type: Types.Url, default: '' },
   portfolio: { type: Types.Url, default: '' },
-  tech: {
-    intro: { type: Types.Number, default: 0 },
-    front: { type: Types.Number, default: 0 },
-    prototyping: { type: Types.Number, default: 0 },
-    prototyping_2: { type: Types.Number, default: 0 },
-    js_next_steps: { type: Types.Number, default: 0 }
-  },
-  life: {
-    attendance: { type: Types.Number, default: 0 },
-    comunication: { type: Types.Number, default: 0 },
-    excellency: { type: Types.Number, default: 0 },
-    stress: { type: Types.Number, default: 0 },
-    teamwork: { type: Types.Number, default: 0 }
-  },
   english: { type: Types.Select, default: 'elemental', options: ['elemental', 'basic', 'intermediate', 'advanced'] },
   resume: {
     doc: { type: Types.Url },
     pic: { type: Types.CloudinaryImage }
-  }
+  },
+  working: { type: Types.Boolean, default: false },
+  course: { type: Types.Relationship, ref: 'Course' }
 })
 
 Coder.defaultColumns = 'name, email'
