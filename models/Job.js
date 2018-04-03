2ee605977b92e95a87ff371c83a8953b6051a412
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2018-04-03 13:56:27 -0300

    fix required fields for jobs

--- models/Job.js
+++ models/Job.js
@@ -5,15 +5,15 @@ var Job = new keystone.List('Job')
 
 Job.add({
   company: { type: Types.Text, required: true, default: '' },
-  address: { type: Types.Text, required: true, default: '' },
+  address: { type: Types.Text, default: '' },
   companyDescription: { type: Types.Textarea, required: true, default: '' },
-  taxId: { type: Types.Text, required: true, default: '' },
+  taxId: { type: Types.Text, default: '' },
   techSkills: { type: Types.TextArray, default: [] },
   lifeSkills: { type: Types.TextArray, default: [] },
-  coders: { type: Types.Textarea, required: true, default: '' },
+  coders: { type: Types.Textarea, default: '' },
   contactName: { type: Types.Text, required: true, default: '' },
   contactEmail: { type: Types.Text, required: true, default: '' },
-  contactPhone: { type: Types.Text, required: true, default: '' }
+  contactPhone: { type: Types.Text, default: '' }
 });
 
 Job.register()
